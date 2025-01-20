6f2eb045d9c692782ebf974957cc8577640a1311
Yerovy Espitia <luyeesgar@gmail.com>
2025-01-20 08:53:05 -0500

    feat: add create_task + add actions in main

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,33 @@
 use std::io;
 
 fn main() {
+    loop {
+        let mut action = String::new();
+        println!("What do you want to do?");
+
+        io::stdin()
+            .read_line(&mut action)
+            .expect("Failed to read line");
+
+        if action.trim() == "break" {
+            break;
+        }
+
+        match action.as_ref() {
+            "create" => create_task(),
+            _ => println!("No valid option"),
+        }
+    }
+}
+
+fn create_task() {
     println!("Please enter a title: ");
 
+    // Declare mutable string variables
     let mut title = String::new();
     let mut description = String::new();
 
+    // Enter input
     io::stdin()
         .read_line(&mut title)
         .expect("Failed to read line");
@@ -16,6 +38,7 @@ fn main() {
         .read_line(&mut description)
         .expect("Failed to read line");
 
-    println!("Title: {title}");
-    println!("Description: {description}");
+    // Print new task
+    println!("\nNew task added");
+    println!("\nTitle: {}Description: {}", title, description);
 }
